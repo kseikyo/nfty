b662596a829eb13c72f045394c742b3ac99b4168
Josh Pigford <josh@joshpigford.com>
2021-10-16 12:20:41 -0500

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,66 @@
+import { useWeb3React } from '@web3-react/core'
+import axios from 'axios'
 import Head from 'next/head'
+import { injected } from '../components/wallet/connectors'
+
+await axios.post('/api/auth/login', {
+  address: await web3.getSigner().getAddress(),
+  signature: await web3.getSigner().signMessage(message),
+})
 
 export default function Home() {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+
+  async function connect() {
+    try {
+      await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate()
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-12">
       <Head>
-        <title>Create Next App</title>
+        <title>NFTY</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://kit.fontawesome.com/d36e6b2199.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+      <div className="sm:mx-auto sm:w-full sm:max-w-lg">
+        <p className="text-center text-green-500 text-8xl"><i class="fa-duotone fa-poo-storm"></i></p>
+        <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">NFTY</h2>
+        <p className="mt-2 text-sm text-center text-gray-600">
+          Tools for your NFTs
         </p>
+      </div>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
+        <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+          <p className="mb-5">
+            <button onClick={connect}
+              type="submit"
+              className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            >
+              Connect to MetaMask
+            </button>
+          </p>
+          <p className="text-center">
+            {active ? <span>Connected with <b>{account}</b></span> : <span> No wallet currently connected</span>}
+          </p>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
